f399c3064883eefdcdad40f11c2a4371cb675968
Kibeyefa Koboju <kobojuezekiel@gmail.com>
2025-04-09 09:34:49 +0100

    ..

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, "/home/qqjjkxng/test.lumina.com.ng/backend")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shutter9ja.settings")
 
